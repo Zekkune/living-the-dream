8e2fab211ce5f3434c23c0886f6892e6c491236e
Zekkune <christian.adams22405@gmail.com>
2024-03-10 22:13:10 -0400

    added models

--- living-the-dream/backend/models/listing.js
+++ living-the-dream/backend/models/listing.js
@@ -4,6 +4,11 @@ const { Schema } = require('mongoose')
 
 const Listing = new Schema(
 {
+    listing_id : {type: mongoose.Schema.Types.ObjectId},
+    title : {type: String, required: true},
+    description : {type: String, required: true},
+    rating : {type : Number, required: true},
+    img : {type : String, required: true}       
 
 },
 {timestamps: true} )
