ff8ce57ceb6d6802a588ba1fd293e48bc2090a41
Zekkune <christian.adams22405@gmail.com>
2024-03-14 18:16:22 -0400

    mapped data for rendering

--- living-the-dream/src/Components/SearchBar.jsx
+++ living-the-dream/src/Components/SearchBar.jsx
@@ -1,34 +1,25 @@
 import { useState } from "react"
 import axios from 'axios'
-
-
+import { BASE_URL } from "../global";
 export default function SearchBar (props) {
-
 const [searchInput, setSearchInput] = useState ("")
-
 const handleChange = (e) => {
-    e.preventDefault();
-    setSearchInput(e.target.value);
-  };
-
-  const handleSearch = (e) => {
-    e.preventDefault();
-      axios.get('mongodb://127.0.0.1:27017/dreamDatabase').then((response) => {
-        props.setapiResponse(response.data);
-      });
-  };
-  
-
-    return (
-        <div className= "searchBar"> 
-
-        <h4>Type here</h4>
-        <input type= "text" placeholder="Search Here"></input>
-        {/* <input type= "text" placeholder="Search Here" onChange={handleChange} value={searchInput}></input> */}
-
-        </div>
-
-
-
-    )
+  e.preventDefault();
+  setSearchInput(e.target.value);
+  // console.log(searchInput)
+ };
+ const handleSearch = async (e) => {
+  e.preventDefault();
+  console.log(e.target.searchInput.value)
+   const response = await axios.get(`${BASE_URL}/listings/${e.target.searchInput.value}`)
+   console.log(response.data)
+ };
+  return (
+    <div className= "searchBar">
+      <form onSubmit={handleSearch}>
+      <input type= "text" placeholder="Be Enchanted!" id='searchBar' onChange={handleChange} name='searchInput' value={searchInput}></input>
+      </form>
+    
+    </div>
+  )
 }
