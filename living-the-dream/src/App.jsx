b0d09d034114fa1d9c10c8def350be9d07e04890
Ameyzin04 <ameyo.kugbe@gmail.com>
2024-03-14 18:07:26 +0000

    Search bar edit

--- living-the-dream/src/App.jsx
+++ living-the-dream/src/App.jsx
@@ -2,10 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Categories from './Components/Categories'
-import DisneyCategory from './Components/DisneyCategory'
-import MarvelCategory from './Components/MarvelCategory'
-import LotrCategory from './Components/LotrCategory'
-import HorrorCategory from './Components/HorrorCategory'
 import Home from './Components/Home'
 import Footer from './Components/Footer'
 import { Route , Routes } from 'react-router-dom'
@@ -22,11 +18,6 @@ const App = () => {
       <Route path= '/TeamDream' element={<TeamDream />}/>
       <Route path= '/SurpriseMer' element={<SurpriseMe/>}/>
       <Route path= '/MostPopular' element={<MostPopular/>}/>
-      <Route path="/" exact component={Categories} />
-      <Route path="/categories/disney" component={DisneyCategory} />
-      <Route path="/categories/marvel" component={MarvelCategory} />
-      <Route path="/categories/lotr" component={LotrCategory} />
-      <Route path="/categories/horror" component={HorrorCategory} />
     </Routes>
     <Home />
     <Footer />
--- living-the-dream/src/Components/SearchBar.jsx
+++ living-the-dream/src/Components/SearchBar.jsx
@@ -1,16 +1,29 @@
 import { useState } from "react"
+import axios from 'axios'
 
-export default function SearchBar () {
 
-    // const [searchInput, setSearchInput] = useState ("")
+export default function SearchBar (props) {
 
+const [searchInput, setSearchInput] = useState ("")
+
+const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+      axios.get('mongodb://127.0.0.1:27017/dreamDatabase').then((response) => {
+        props.setapiResponse(response.data);
+      });
+  };
+  
 
     return (
         <div className= "searchBar"> 
-
-        <h4>Type here</h4>
-        <input type= "text" placeholder="Search Here"></input>
-        {/* <input type= "text" placeholder="Search Here" onChange={handleChange} value={searchInput}></input> */}
+        
+        <input type= "text" placeholder="Search Here" id='searchBar' onChange={handleChange} value={searchInput}></input>
+        <button onSubmit={handleSearch}>Search Here</button>
 
         </div>
 
